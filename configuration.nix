58abf3cdad41214cc6432a8322ac7a004336c6c2
220 <mjlwbb0a1e@proton.me>
2026-02-23 16:09:57 +0330

    FIX: openssh mentioned twice

--- configuration.nix
+++ configuration.nix
@@ -80,10 +80,9 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
    environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      wget
      git
-     openssh
      kitty
      alacritty
    ];
