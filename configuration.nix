17efb4e4662ad1fe0ee7c6c112caf94958410cf5
220 <mjlwbb0a1e@proton.me>
2026-02-27 08:06:07 +0330

    Changed the bootloader to grub
    the configuration wasnt booting in QEMU

--- configuration.nix
+++ configuration.nix
@@ -11,13 +11,13 @@
     ];
 
   # Use the GRUB 2 boot loader.
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
   # boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-   boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
    boot.loader.efi.canTouchEfiVariables = true;
 
 
